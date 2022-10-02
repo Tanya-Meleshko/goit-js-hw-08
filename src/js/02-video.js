557b7702d3c4922717dc9c08e7e5d51611be996b
Tanya-Meleshko <melehhha@gmail.com>
2022-10-02 17:00:40 +0300

    Solve task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  Throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(currentTime);
